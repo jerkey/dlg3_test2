e4c6469d61eeb72eb4ac454cb42d652beb7bbf23
jake waters <jerkey@g-cipher.net>
2015-03-24 16:38:40 -0700

    merge charging controls from branch testcharge

--- dlg3_test2.ino
+++ dlg3_test2.ino
@@ -2,11 +2,12 @@
 #define ONFET        4  // PD4/4
 #define CCFL_PIN     5  // pin 10 old, pin 5 new (for 32khz special)
 #define LED_PIN      7  // PD7/7
-#define CHARGE123    9  // PB1/9 prototype
+// #define CHARGE123    9  // PB1/9 prototype
 #define DRAIN1       11 // PB3/11 prototype
 #define DRAIN2       3  // PD3/3
 #define DRAIN3       10 // PB2/10 prototype
 #define BOOST        6  // PD6/6 prototype
+#define CHARGE123    BOOST // we changed the board
 #define B1THERM      A0
 #define B2THERM      A1
 #define B3THERM      A2
@@ -15,11 +16,16 @@
 #define BATTERY      A5
 #define CCFL_SENSE   A6
 #define JACK_SENSE   A7
+// TODO: test with actual charging
+// TODO: write charging into master branch
+// TODO: write balancing algorithm in master branch
 
 #define B1P_COEFF     203.518 // ADC counts per volt
 #define B2P_COEFF     122.460
 #define BATTERY_COEFF 81.936
-#define BATT_EMPTY   2.95 // how many volts to DIE at
+#define BATT_EMPTY   2.75 // how many volts to DIE at
+#define JACK_COEFF (BATTERY_COEFF ) // using same sense resistors but..
+#define R901_OHMS 0.462 / 0.92 // ohms of R between JACK_SENSE and BATTERY
 
 int brightness = 450; // 175 with built-in vref
 #define MAX_PWM 250
@@ -28,31 +34,41 @@ int jumpVal = 1;
 int offCount = 0;  // counts how many off requests we've seen
 #define OFF_THRESH 5 // how many to make us turn off
 unsigned long senseRead = 0;
-#define DELAYFACTOR 64 // millis() and delay() this many times faster
-float batt1,batt2,batt3; // voltage of battery cells
-int debugMode = 0; // allows for debugging modes to be triggered in production software
+#define DELAYFACTOR 32 // millis() and delay() this many times faster
+float batt1,batt2,batt3,battery_total ; // voltage of battery cells and total
+float r901, chargeI; // voltage at R901_OHMS, current across it
+unsigned short debugMode = 0, lightMode = 0; // allows for debugging modes to be triggered in production software
+unsigned short drainPWM[3] = {0,0,0}; // individual battery balancers, 254 = ON
+unsigned short lastDrainPWM[3] = {0,0,0}; // to prevent unnecessary analogWrites
+int chargePWM=255; // charge123 is a PFET, 255 = none, 254=min 1=max charging
+int lastChargePWM = 0; // different so updatePWM() sets it immediately
 
 void setup() {
 pinMode(ONFET,OUTPUT);
 digitalWrite(ONFET,HIGH);  // keep power on
+pinMode(CHARGE123,OUTPUT);
+pinMode(DRAIN1   ,OUTPUT);
+pinMode(DRAIN2   ,OUTPUT);
+pinMode(DRAIN3   ,OUTPUT);
 
 pinMode(LED_PIN,OUTPUT);
 digitalWrite(LED_PIN,HIGH);  // LED ON
 
-pinMode(CCFL_PIN,OUTPUT); // 32khz
-// setPwmFrequency(CCFL_PIN,1); // set PWM freq to 31,250 Hz
+pinMode(CCFL_PIN,OUTPUT); // 15.63khz
+setPwmFrequency(DRAIN3,8); // timer1 = pin 9,10 = OLD CHARGE123, DRAIN3
+setPwmFrequency(DRAIN2,8); // timer2 = pin 3,11 = DRAIN2, DRAIN1
 // WGM02 = 0, WGM01 = 1, WGM00 = 1 see page 108
 // COM0B1 = 1, COM0B0 = 0 see page 107
 // CS02 = 0, CS01 = 0, CS00 = 1 see page 110
   CLKPR = 0x80;  // enable write to clkps see page 37
   CLKPR = 0x00;  // set divisor to 1  see page 38
-  TCCR0A = 0b10100011;
+  TCCR0A = 0b10100011; // timer0 affects pin 5,6 = CCFL_PIN,BOOST
   TCCR0B = 0b00000001;
   Serial.begin(76800); // to get 38400 baud, put 76800 baud here
 }
 
 void loop() {
-  if (debugMode == 0) {
+  if (lightMode == 0) {
     senseRead = averageRead(CCFL_SENSE);
     if (senseRead < brightness) {
       digitalWrite(LED_PIN,HIGH);  // LED ON
@@ -66,6 +82,8 @@ void loop() {
       if (pwmVal < 1) pwmVal = 1;
       analogWrite(CCFL_PIN,pwmVal);
     }
+  } else {
+    analogWrite(CCFL_PIN,0);
   }
   getBattVoltages();
   printAnalogs();
@@ -84,6 +102,7 @@ void loop() {
   }
 
   if (offCount > OFF_THRESH) die("offCount > OFF_THRESH");
+  updateDrains(); // make sure PWMs are set
 }
 
 float averageRead(int pin) {
@@ -96,16 +115,20 @@ float averageRead(int pin) {
 void getBattVoltages() {
   batt1 = averageRead(B1P) / B1P_COEFF;
   batt2 = (averageRead(B2P) / B2P_COEFF) - batt1;
-  batt3 = (averageRead(BATTERY) / BATTERY_COEFF) - batt2 - batt1;
+  battery_total = averageRead(BATTERY) / BATTERY_COEFF;
+  batt3 = battery_total - batt2 - batt1;
+  r901 = averageRead(JACK_SENSE) / JACK_COEFF;
+  chargeI = (r901 - battery_total) / R901_OHMS;
 }
 
 void die(String reason) {
   Serial.println("off because: "+reason);
-  digitalWrite(LED_PIN,HIGH);  // LED ON
+  digitalWrite(LED_PIN,LOW);  // LED OFF
   digitalWrite(ONFET,LOW); // turn power off
   analogWrite(CCFL_PIN,0); // turn off ccfl
   while (true) { // wait here until they let go of button
     Serial.print(reason);
+    updateDrains(); // make sure PWMs are set
     getBattVoltages();
     printAnalogs();
     delay((unsigned)1000*DELAYFACTOR);
@@ -117,22 +140,38 @@ void printAnalogs() {
   Serial.print("  ");
   if (debugMode > 0) { // print detailed battery analogRead information
     Serial.print(averageRead(B1P),3);
-    Serial.print(" B1P  (");
+    Serial.print(" B1P (");
     Serial.print(averageRead(B1P)/B1P_COEFF,3);
-    Serial.print(")   ");
+    Serial.print(")  ");
     Serial.print(averageRead(B2P),3);
-    Serial.print(" B2P  (");
+    Serial.print(" B2P (");
     Serial.print(averageRead(B2P)/B2P_COEFF,3);
-    Serial.print(")   ");
+    Serial.print(")  ");
     Serial.print(averageRead(BATTERY),3);
-    Serial.print(" BATTERY  (");
-    Serial.print(averageRead(BATTERY)/BATTERY_COEFF,3);
-    Serial.print(")      TH1: ");
-    Serial.print(averageRead(B1THERM),2);
-    Serial.print("  TH2: ");
-    Serial.print(averageRead(B2THERM),2);
-    Serial.print("  TH3: ");
-    Serial.print(averageRead(B3THERM),2);
+    Serial.print(" BATTERY (");
+    float battery_print = averageRead(BATTERY)/BATTERY_COEFF;
+    Serial.print(battery_print,3);
+    Serial.print(")  ");
+    float jack_print = averageRead(JACK_SENSE)/JACK_COEFF;
+    Serial.print(jack_print,3);
+    Serial.print(" R901 (");
+    Serial.print(averageRead(JACK_SENSE),3);
+    Serial.print(")  CHARGEI: ");
+    Serial.print((jack_print - battery_print) / R901_OHMS,2);
+    Serial.print("  CHARGE123: ");
+    Serial.print(chargePWM);
+    Serial.print("  DRAIN1: ");
+    Serial.print(drainPWM[0]);
+    Serial.print("  DRAIN2: ");
+    Serial.print(drainPWM[1]);
+    Serial.print("  DRAIN3: ");
+    Serial.print(drainPWM[2]);
+    Serial.print("  TH1:");
+    Serial.print(averageRead(B1THERM),1);
+    Serial.print("  TH2:");
+    Serial.print(averageRead(B2THERM),1);
+    Serial.print("  TH3:");
+    Serial.print(averageRead(B3THERM),1);
   } else {
     Serial.print("batt1=");
     Serial.print(batt1,3);
@@ -140,9 +179,31 @@ void printAnalogs() {
     Serial.print(batt2,3);
     Serial.print("   batt3=");
     Serial.print(batt3,3);
+    Serial.print("  CHARGEI: ");
+    Serial.print(chargeI,2);
   }
   Serial.println("");
 }
+
+void updateDrains() {
+  if (drainPWM[0] != lastDrainPWM[0]) {
+    analogWrite(DRAIN1,drainPWM[0]);
+    lastDrainPWM[0]=drainPWM[0];
+  }
+  if (drainPWM[1] != lastDrainPWM[1]) {
+    analogWrite(DRAIN2,drainPWM[1]);
+    lastDrainPWM[1]=drainPWM[1];
+  }
+  if (drainPWM[2] != lastDrainPWM[2]) {
+    analogWrite(DRAIN3,drainPWM[2]);
+    lastDrainPWM[2]=drainPWM[2];
+  }
+  if (chargePWM != lastChargePWM) {
+    analogWrite(CHARGE123,chargePWM);
+    lastChargePWM=chargePWM;
+  }
+}
+
 
 void setPwmFrequency(int pin, int divisor) {
   byte mode;
